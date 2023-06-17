44c2d661c699a63ce3f57b515bb7ff41a2a797e5
Savio Kane <savio.kanesk@gmail.com>
2023-06-17 19:42:01 -0300

    página personagens ok

--- javascript/personagens.js
+++ javascript/personagens.js
@@ -1,21 +1,20 @@
+function abrirVideo(id) {
+    var imagem = document.getElementById(id + "-imagem");
+    var video = document.getElementById(id + "-video");
+  
+    imagem.style.display = "none";
+    video.style.display = "block";
+    video.play();
+  }
+  
+  function fecharVideo(id) {
+    var video = document.getElementById(id + "-video");
+    var imagem = document.getElementById(id + "-imagem");
+  
+    imagem.style.display = "block";
+    video.style.display = "none";
+    video.pause();
+  }
 
-function abrirVideo(){
-    var imagem = document.getElementById("astra-imagem");
-    var video = document.getElementById("astra-video");
-    if(imagem.src.match("./imagens/personagens/astra.jpg")){
 
-        imagem.style.display = "none";
-        video.style.display = "block" ;
-        video.play();
-    }
-}
-    function fecharVideo(){
-        var video = document.getElementById("astra-video");
-        var imagem = document.getElementById("astra-imagem");
-        if(video.src.match("./imagens/personagens/astra-video.mp4")){
-            imagem.style.display = "block";
-            video.style.display = "none";
-        }
-
-    }
 
